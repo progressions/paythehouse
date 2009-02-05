31cbcf1217911e7896367b376ef5233f1670cde6
Jeff Coleman <jeffcoleman@jeff.local>
2009-02-05 16:41:50 -0600

    Fixing error in app.html.erb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,28 @@ config.action_controller.perform_caching             = true
 # Restful Authentication
 REST_AUTH_SITE_KEY = 'f5945d1c74d3502f8a3de8562e5bf21fe3fec887'
 REST_AUTH_DIGEST_STRETCHES = 10
+
+config.action_controller.cache_store = [:file_store, "#{RAILS_ROOT}/tmp/cache"]
+
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host                  = "http://assets.example.com"
+
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
+
+# Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = false
+
+APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")['mailer']
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  :address  => APP_CONFIG['address'],
+  :port  => APP_CONFIG['port'], 
+  :domain  => APP_CONFIG['domain'],
+  :user_name  => APP_CONFIG['user_name'],
+  :password  => APP_CONFIG['password'],
+  :authentication  => :login
+}
+
